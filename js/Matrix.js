eca910cf8c252cdbbb9f81b5a4b3d3ff8edd8bae
Alec Lawlor <lawlorab@bc.edu>
2019-10-28 17:44:53 -0400

    final for lab

--- js/Matrix.js
+++ js/Matrix.js
@@ -1,15 +1,115 @@
-
 export default function Matrix(){
 	// init size
 	let margin ={ top: 100, right: 20, bottom: 20, left: 100 };
 	let size = 600;
 
     let pos = d3.scaleBand();
-
     function chart(selection){
+
         selection.each(function(data){
-            // follow the instruction to fill in this part
+
+            pos.domain(d3.range(data.length))
+            .rangeRound([0, size-margin.left - margin.right])
+            .paddingInner(0.25)
+
+            let svg = d3.select(this).selectAll('svg')
+                .data([data])
+
+            let svgEnter = svg.enter().append('svg')
+            svgEnter.append('g')
+
+            svg = svg.merge(svgEnter)
+
+            svg.attr('width', size).attr('height', size)
+
+            let g = svg.select('g')
+                .attr('transform', "translate(" + margin.left + "," + margin.top + ")")
+
+            // draw matrix rows (and y-axis labels)
+            let dataJoin = g.selectAll('.matrix-row')
+                .data(data, d => d.Family)
+
+            let rowGroups = dataJoin.enter()
+                .append('g')
+                .attr('class', 'matrix-row')
+
+            rowGroups.merge(dataJoin)
+            .style('opacity', .5)
+            .transition()
+            .duration(1000)
+            .style('opacity', 1)
+            .attr('transform', (d, i) => "translate(0," + pos(i) + ")")
+
+            // draw marriage triangles within each group
+
+            rowGroups.selectAll('.matrix-cell')
+                .data(d => d.cols)
+                .enter().append('rect')
+                .attr('x', (d, i) => pos(i))
+                .attr('width', pos.bandwidth())
+                .attr('height', pos.bandwidth())
+                .attr('fill', d => d.marriage == 0 ? "#ddd" : "#8686bf")
+
+            g.selectAll('.matrix-column-label')
+                .data(data).enter().append("text")
+                .attr('class', 'matrix-column-label')
+                .attr('alignment-baseline', 'middle')
+                .attr('transform', function(d, i){
+                    return "translate(" +( pos(i)+ pos.bandwidth()/2)+ ",-8) rotate(270)"
+                })
+                .text(d => d.Family)
+            
+            // row and column labels
+            rowGroups.append('text')
+                .attr('class', 'matrix-row-label')
+                .attr('y', pos.bandwidth()/2)
+                .attr('dx', -10)
+                .attr('text-anchor', 'end')
+                .attr('alignment-baseline', 'middle')
+                .text(d => d.Family)
+            
+                rowGroups.selectAll(".matrix-cell-marriage")
+                .data(d=> d.cols)
+                .enter().append("path") // only use enter since the matrix size does not change.
+                .attr("class", "matrix-cell-marriage")
+                .attr('d', (d, i)=>'M ' + pos(i) +' '+ 0 + ' l ' + pos.bandwidth() + ' 0 l 0 ' + pos.bandwidth() + ' z')
+                .attr("fill", d=>d.marriage == 0 ? "#ddd" : "#8686bf")
+                .attr('class', 'matrix-cell matrix-cell-marriage')
+                .on('mouseover', handleMouseoverCell)
+                .on('mouseout', handleMouseoutCell)
+
+                rowGroups.selectAll(".matrix-cell-business")
+                .data(d=> d.cols)
+                .enter().append("path")
+                .attr("class", "matrix-cell matrix-cell-business")
+                .attr('d', (d,i)=>'M ' + pos(i) +' '+ 0 + ' l 0 ' + pos.bandwidth() + ' l ' + pos.bandwidth() + ' 0 z')
+                .attr("fill", d=>d.business == 0 ? "#ddd" : "#fbad52")
+                .attr('class', 'matrix-cell matrix-cell-business')
+                .on('mouseover', handleMouseoverCell)
+                .on('mouseout', handleMouseoutCell)
+
         });
+
+        function handleMouseoverCell(d,i){
+            let row = d3.select(this.parentNode).datum().index; // parent: row
+            let col = i;
+            let g = d3.select(this.parentNode.parentNode);// get group
+        
+            g.selectAll(".matrix-cell")
+                .filter(d=>d.r!==row&&d.c!==col)
+                .transition()
+                .duration(200)
+                .attr("fill-opacity", 0.2);
+        };
+
+        function handleMouseoutCell(){
+            let g = d3.select(this.parentNode.parentNode);// get group
+            g.selectAll(".matrix-cell")
+                .transition()
+                .duration(200)
+                .attr("fill-opacity", 1);
+            };
+
     }
     return chart;
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,62 @@
-
+import Matrix from './Matrix.js';
+let matrix = Matrix();
+let viewData;
 
 // Use queue.js to read the two datasets asynchronous
 d3.json('data/florentine-family.json').then(data=>{
+
+    function transform(data){
+
+        let rows = [],
+            n=data.nodes.length;
+
+        rows = data.nodes.map((node, i) => ({
+            ...node,
+            index: i,
+            numMarriages: 0,
+            numBusinessTies: 0,
+            numAllRelations: 0,
+            cols: d3.range(n).map(j => ({r:i, c:j, marriage: 0, business: 0}))
+        }))
+
+        data.links.forEach(function(link){
+            rows[link.source].numAllRelations += 1
+            rows[link.target].numAllRelations += 1
+
+            if(link.type === 'marriage'){
+                rows[link.source].cols[link.target].marriage = 1;
+                rows[link.source].numMarriages += 1;
+                rows[link.target].numMarriages += 1;
+            }
+
+            if(link.type === 'business'){
+                rows[link.source].cols[link.target].business = 1;
+                rows[link.source].numBusinessTies += 1;
+                rows[link.target].numBusinessTies += 1;
+            }
+        })
+        console.log(rows)
+        return rows
+    }
+    
+    document.querySelector('#select-order-type').addEventListener('change', function(){
+        let orderingType = this.value
+        d3.select('#chart-area')
+            .call(matrix)
+        viewData.sort(function(a, b){
+            if (orderingType == 'index'){
+                return a[orderingType] - b[orderingType]
+            }
+            else{
+                return b[orderingType] - a[orderingType]
+            }
+        })
+    })
+
+    viewData = transform(data)
+    d3.select('#chart-area')
+        .datum(viewData)
+    .call(matrix)
 
 });
 
